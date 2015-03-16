7ec9d74f7f1c7e7b662afa17b62c1f3c9d933e9e
spiridonov-oa <spiridonov.oa@gmail.com>
2015-03-16 12:23:46 +0200

    Add animation on projects page

--- public/modules/projects/directives/projects.directive.js
+++ public/modules/projects/directives/projects.directive.js
@@ -11,27 +11,27 @@ angular.module('projects').directive('projects',
                 scope.projects = [
                     {
                         name: 'Project 1',
-                        image: '/img/projects/no-image.jpg',
+                        image: 'img/projects/icons_project/projects_commerce/3icons.jpg',
                         position: 1
                     },
                     {
                         name: 'Project 2',
-                        image: '/img/projects/no-image.jpg',
+                        image: 'img/projects/icons_project/projects_commerce/4icons.jpg',
                         position: 2
                     },
                     {
                         name: 'Project 3',
-                        image: '/img/projects/no-image.jpg',
+                        image: 'img/projects/icons_project/projects_commerce/5icons.jpg',
                         position: 3
                     },
                     {
                         name: 'Project 4',
-                        image: '/img/projects/no-image.jpg',
+                        image: 'img/projects/icons_project/projects_commerce/6icons.jpg',
                         position: 4
                     },
                     {
                         name: 'Project 5',
-                        image: '/img/projects/no-image.jpg',
+                        image: 'img/projects/icons_project/projects_commerce/7icons.jpg',
                         position: 5
                     }
                 ];
