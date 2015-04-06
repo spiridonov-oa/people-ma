d1449568c6bed786a052e97e3aee22db900b475d
spiridonov-oa <spiridonov.oa@gmail.com>
2015-04-06 13:19:27 +0200

    Add fonts for build

--- public/modules/core/directives/state-class.directive.js
+++ public/modules/core/directives/state-class.directive.js
@@ -2,17 +2,42 @@
 
 angular.module('core').directive('stateClass', ['$state', '$rootScope', function ($state, $rootScope) {
 
+    function animation (element, fromState, toState) {
+        var state = '';
+        var animationTime = 1000;
+        if (fromState.name) {
+            state += fromState.name + '-';
+        }
+        state += toState.name;
+
+        switch(state) {
+            case 'home':
+                homeAnimation(fromState);
+                break;
+
+        }
+
+        function homeAnimation () {
+            element.css('opacity', 0);
+            jQuery(element).animate({
+                opacity: 1
+            }, animationTime);
+            console.log('homeAnimation');
+        }
+    }
+
     function link (scope, element, attrs) {
         function stateChangeStartHandler (e, toState, toParams, fromState, fromParams) {
             scope.body = scope.body || {};
+            scope.body.class = '';
 
             if(fromState.name) {
-                scope.body.class = fromState.name + '-' + toState.name + ' ' + toState.name;
-            } else {
-                scope.body.class =  toState.name;
+                scope.body.class = ' from-' + fromState.name + ' ';
             }
 
+            scope.body.class += toState.name + ' ';
             //console.log('scope.body.class: ' + scope.body.class);
+            animation(element, fromState, toState);
         }
 
         $rootScope.$on('$stateChangeStart', stateChangeStartHandler);
