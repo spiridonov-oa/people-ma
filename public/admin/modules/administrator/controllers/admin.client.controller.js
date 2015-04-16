78fb6e1bc1ff69d328d07084cca042a5964ab592
spiridonov-oa <spiridonov.oa@gmail.com>
2015-04-16 04:27:23 +0200

    Remove unnecessary files

--- public/admin/modules/administrator/controllers/admin.client.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('admin').controller('AdminController', ['$scope', '$state', 'Authentication',
-	function($scope, $state, Authentication) {
-		// This provides Authentication context.
-		$scope.authentication = Authentication;
-
-	}
-]);
