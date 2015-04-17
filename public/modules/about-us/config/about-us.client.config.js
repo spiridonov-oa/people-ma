88b1a44a35d67d5835dd25eb45feb9f5d8b3b7e7
spiridonov-oa <spiridonov.oa@gmail.com>
2015-04-17 19:07:31 +0200

    Remove about-us

--- public/modules/about-us/config/about-us.client.config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Configuring the Articles module
-angular.module('about-us').run(['Menus',
-	function(Menus) {
-		// Set top bar menu items
-        Menus.addMenuItem('mainmenu', 'About Us', 'about-us', 'left-margin', '/about-us', true, null, 3);
-	}
-]);
--- public/modules/about-us/controllers/about-us.client.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Projects controller
-angular.module('about-us').controller('AboutUsController', ['$scope', '$stateParams', '$state', '$location', 'Authentication',
-	function($scope, $stateParams, $state, $location, Authentication) {
-		$scope.authentication = Authentication;
-
-	}
-]);
