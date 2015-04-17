bb9d625793cb740767f99fda55bc6baf60dfc6cb
spiridonov-oa <spiridonov.oa@gmail.com>
2015-04-17 19:01:54 +0200

    Remove from admin-menu

--- public/modules/articles/config/articles.client.config.js
+++ public/modules/articles/config/articles.client.config.js
@@ -4,8 +4,8 @@
 angular.module('articles').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('admin-menu', 'Articles', 'articles', 'dropdown', '/articles(/create)?');
+		/*Menus.addMenuItem('admin-menu', 'Articles', 'articles', 'dropdown', '/articles(/create)?');
 		Menus.addSubMenuItem('admin-menu', 'articles', 'List Articles', 'articles');
-		Menus.addSubMenuItem('admin-menu', 'articles', 'New Article', 'articles/create');
+		Menus.addSubMenuItem('admin-menu', 'articles', 'New Article', 'articles/create');*/
 	}
 ]);
--- public/modules/concepts/config/concepts.client.config.js
+++ public/modules/concepts/config/concepts.client.config.js
@@ -4,8 +4,8 @@
 angular.module('concepts').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('admin-menu', 'Concepts', 'concepts', 'dropdown', '/concepts(/create)?');
+		/*Menus.addMenuItem('admin-menu', 'Concepts', 'concepts', 'dropdown', '/concepts(/create)?');
 		Menus.addSubMenuItem('admin-menu', 'concepts', 'List Concepts', 'concepts');
-		Menus.addSubMenuItem('admin-menu', 'concepts', 'New Concept', 'concepts/create');
+		Menus.addSubMenuItem('admin-menu', 'concepts', 'New Concept', 'concepts/create');*/
 	}
 ]);
