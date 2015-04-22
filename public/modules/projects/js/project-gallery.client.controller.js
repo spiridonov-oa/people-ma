fcb32a2cb270d5b8afb9dc1a0bd029dd4933e63f
spiridonov-oa <spiridonov.oa@gmail.com>
2015-04-22 18:14:22 +0200

    Setup slider

--- public/modules/projects/js/project-gallery.client.controller.js
+++ public/modules/projects/js/project-gallery.client.controller.js
@@ -11,9 +11,34 @@ angular.module('projects').controller('ProjectGalleryController', ['$scope', '$s
             $state.go($state.previous.name);
         };
 
+        $scope.carousel = {};
+        $scope.carousel.index = 0;
 
         $scope.project = {};
-        $scope.project.photos = ['/img/projects/pr/pr1.png', '/img/projects/pr/pr2.png'];
+        $scope.project.photos = [];
+        for(var i = 1; i <= 32; i++) {
+            $scope.project.photos.push('/img/projects/pr/pr_' + i + '.jpg');
+        }
+
+        $scope.carousel.nextSlide = function () {
+             if ($scope.carousel.index < $scope.project.photos.length) {
+                 $scope.carousel.index += 1;
+             } else {
+                // $scope.carousel.index = 0;
+             }
+        };
+
+        $scope.carousel.prevSlide = function () {
+            if ($scope.carousel.index > 0) {
+                $scope.carousel.index -= 1;
+            } else {
+               // $scope.carousel.index = $scope.project.photos.length;
+            }
+        };
+
+        $scope.carousel.getPrevImage = function (){
+
+        };
 
 /*
         $scope.findById = function (objId) {
